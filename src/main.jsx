f50c3865759eef8fdb20ebe14104d6510ad154b5
Gbenga Oluwadahunsi <gbengaoluwadahunsi01@gmail.com>
2024-07-20 14:55:50 +0800

    welcome greeting in the developer tool added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-console.log("My name is Gbenga Oluwadahunsi. I am a software developer specializing in building AI-powered applications. Check out my works at this URL: https://gbengaoluwadahunsi.vercel.app");
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
